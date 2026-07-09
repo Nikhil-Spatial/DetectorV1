227458e8ec96f9dce10b44262bada619869cc498
Nikhil Singh <nikhils1220@gmail.com>
2026-07-09 09:11:13 -0700

    Updated loss function

--- src/loss_fn.py
+++ src/loss_fn.py
@@ -1,6 +1,6 @@
 from configs import LAMBDA_COORD, LAMBDA_NOOBJ, S, B
-from uilts.py import IoU
 import torch.nn as nn
+from uilts import IoU
 
 class Loss(nn.Module):
     def __init__(self):
@@ -29,10 +29,11 @@ class Loss(nn.Module):
             for j in list(range(7)):
                 pred_cell = pred[i][j]
                 target_cell = target[i][j]
+                cell_has_object = target_cell[24] == 1 or target_cell[29] == 1
 
-                if target_cell.any():
-                    responsible = _find_responsible(pred_cell, target_cell, i, j)
-                    pred_bbox = pred_cell[20:25] if responsible else pred_cell[25:30]
+                if cell_has_object:
+                    responsible = _find_responsible_(pred_cell, target_cell, i, j)
+                    pred_bbox = pred_cell[20:25] if responsible == 0 else pred_cell[25:30]
                     target_bbox = target_cell[20:25]
 
                     # 1. Sum of Squared Errors (SSE) for center points
@@ -40,11 +41,18 @@ class Loss(nn.Module):
                               (target_bbox[1] - pred_bbox[1]).square()
 
                     # 2. SSE for square roots of width and height
-                    loss_2 += (target_bbox[2].sqrt() - pred_bbox[2].sqrt()).square() + \
-                              (target_bbox[3].sqrt() - pred_bbox[3].sqrt()).square()
+                    # avoid errors with negative predictions
+                    pred_w = pred_bbox[2].abs().sqrt() + 1e-6
+                    pred_h = pred_bbox[3].abs().sqrt() + 1e-6
 
-                    # 3. SSE for confidences of cells with objects
+                    loss_2 += (target_bbox[2].sqrt() - pred_w).square() + \
+                              (target_bbox[3].sqrt() - pred_h).square()
+
+                    # 3. SSE for confidences of responsible bounding box predictors
                     loss_3 += (target_bbox[4] - pred_bbox[4]).square()
+
+                    # 4. SSE for confidences of not responsible bounding box predictors
+                    not_responsible_idx = (responsible + 1) % 2
 
                     # 5. SSE for class probabilities
                     for c in list(range(20)):
