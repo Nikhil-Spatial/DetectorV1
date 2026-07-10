7818cdee616130ce8a95898c0a94a2d29b6bca03
Nikhil Singh <nikhils1220@gmail.com>
2026-07-10 12:38:01 -0700

    Updated IoU utility again

--- src/utils.py
+++ src/utils.py
@@ -30,23 +30,23 @@ def area(bbox):
     h = torch.clamp(bbox[3] - bbox[1], min=0)
     return w * h
 
-def IoU(pred, target, row=None, col=None, conversion_needed=False):
+def IoU(bbox_1, bbox_2, row=None, col=None, conversion_needed=False):
     # 1. convert (x, y, w, h) to (xmin, ymin, xmax, ymax) --- if needed
     if conversion_needed:
-        target = convert_xywh_coords(target, row, col, False)
-        pred = convert_xywh_coords(pred, row, col, False)
+        bbox_1 = convert_xywh_coords(bbox_1, row, col, False)
+        bbox_2 = convert_xywh_coords(bbox_2, row, col, False)
 
     # 2. find intersection box coordinates
-    xmin = torch.max(target[0], pred[0])
-    ymin = torch.max(target[1], pred[1])
-    xmax = torch.min(target[2], pred[2])
-    ymax = torch.min(target[3], pred[3])
+    xmin = torch.max(bbox_1[0], bbox_2[0])
+    ymin = torch.max(bbox_1[1], bbox_2[1])
+    xmax = torch.min(bbox_1[2], bbox_2[2])
+    ymax = torch.min(bbox_1[3], bbox_2[3])
 
     inter = (xmin, ymin, xmax, ymax)
 
     # 3. find areas of boxes
-    target_area = area(target)
-    pred_area = area(pred)
+    target_area = area(bbox_1)
+    pred_area = area(bbox_2)
     inter_area = area(inter)
     union_area = target_area + pred_area - inter_area
 
