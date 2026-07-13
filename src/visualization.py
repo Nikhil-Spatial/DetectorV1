c8ae7d3f34310de902e462cf1338a0afd55a2e08
Nikhil Singh <nikhils1220@gmail.com>
2026-07-13 09:10:23 -0700

    updated draw rectangle

--- src/visualization.py
+++ src/visualization.py
@@ -4,6 +4,8 @@ def draw_rectangles(image, coords, labels):
     image = image.permute(1, 2, 0).numpy()
     image = bbv.draw_multiple_rectangles(image, coords, bbox_color=(255, 0, 0),
                                          thickness=1)
-    image = bbv.add_multiple_labels(image, labels, coords)
+    image = bbv.add_multiple_labels(image, labels, coords, size=0.5,
+                                    thickness=1, draw_bg=False, text_color=
+                                    (255, 255, 255))
 
     return image
