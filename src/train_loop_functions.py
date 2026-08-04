ded6a0505d407478dee090c683f123f87c2ea491
Nikhil Singh <nikhils1220@gmail.com>
2026-08-04 14:59:55 -0700

    End of workday

--- src/train_loop_functions.py
+++ src/train_loop_functions.py
@@ -1,5 +1,5 @@
 from src.evaluation import (find_tp_and_fp, count_objects_in_each_class,
-                            average_precision, mean_average_precision)
+                            evaluate)
 from src.postprocessing import postprocess_preds
 from matplotlib import pyplot as plt
 from src.configs import IDX_TO_CLASS
