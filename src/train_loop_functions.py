6fa17aa4d50ec426582c943d84e150b1efda1beb
Nikhil Singh <nikhils1220@gmail.com>
2026-08-05 08:39:38 -0700

    Finished compute evaluation statistics method

--- src/train_loop_functions.py
+++ src/train_loop_functions.py
@@ -1,8 +1,12 @@
 from src.evaluation import (find_tp_and_fp, count_objects_in_each_class,
                             evaluate)
+from src.utilities import (get_ground_truth_objects_by_class,
+                           get_empty_precision_recall_lists,
+                           get_empty_class_object_totals,
+                           get_empty_tp_fp_by_class)
 from src.postprocessing import postprocess_preds
+from src.configs import IDX_TO_CLASS, BATCH_SIZE
 from matplotlib import pyplot as plt
-from src.configs import IDX_TO_CLASS
 from pathlib import Path
 import torch
 
@@ -14,22 +18,67 @@ def train(model, loss_fn, optimizer, train_dl, device):
         X_batch = X_batch.to(device)
         y_batch = y_batch.to(device)
 
-        # 1. Forward Pass
-        preds = model(X_batch)
+        # 1. forward pass
+        preds_batch = model(X_batch)
 
-        # 2. Compute Loss
-        loss = loss_fn(preds, y_batch)
+        # 2. compute loss
+        loss = loss_fn(preds_batch, y_batch)
 
-        # 3. Reset Gradients
+        # 3. reset gradients
         optimizer.zero_grad()
 
-        # 4. Compute Gradients
+        # 4. compute gradients
         loss.backward()
 
-        # 5. Optimizer Step
+        # 5. optimizer step
         optimizer.step()
 
         total_loss += loss.item()
 
     return total_loss / len(train_dl) # returns average loss
 
+def compute_eval_stats(model, dl, device, loss_fn=None):
+    if loss_fn: total_loss = 0
+
+    tp_fp_by_class = get_empty_tp_fp_by_class()
+    class_object_totals = get_empty_class_object_totals()
+    precision_recall_Lists = get_empty_precision_recall_lists()
+
+    model.eval()
+    with torch.no_grad():
+        for X_batch, y_batch in dl:
+            X_batch = X_batch.to(device)
+            y_batch = y_batch.to(device)
+
+            # 1. forward pass
+            preds_batch = model(X_batch)
+
+            # 2. compute validation loss (optional)
+            if loss_fn:
+                val_loss = loss_fn(preds_batch, y_batch)
+                total_val_loss += val_loss.item()
+
+            # 3. a) postprocess predictions, b) sort and bucket ground truth
+            # objects by class, c) find True and False Positives, and d) count
+            # the number of objects in each class
+            for i in range(BATCH_SIZE):
+                postprocessed_preds = postprocess_preds(preds_batch[i])
+                ground_truth_objects_by_class = get_ground_truth_objects_by_class(
+                    y_batch[i]
+                )
+
+                find_tp_and_fp(postprocessed_preds,
+                               ground_truth_objects_by_class, tp_fp_by_class)
+                count_objects_in_each_class(ground_truth_objects_by_class,
+                                            class_object_totals)
+
+        # 4. Compute average precision (AP) for each class and mean average 
+        # precision (mAP) across all classes
+        mAP, ap_by_class = evaluate(tp_fp_by_class, class_object_totals,
+                                    precision_recall_lists)
+
+    if loss_fn:
+        avg_val_loss = total_val_loss / len(dl)
+        return mAP, ap_by_class, avg_val_loss, precision_recall_lists
+
+    return mAP, ap_by_class, avg_val_loss, precision_recall_lists
