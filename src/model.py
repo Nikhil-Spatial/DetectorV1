f9cc101529b3432e65d35e7eb214de4d38615222
Nikhil Singh <nikhils1220@gmail.com>
2026-08-07 13:32:06 -0700

    Implemented transfer learning after learning how to do it and the first principles behind it

--- src/model.py
+++ src/model.py
@@ -1,62 +1,9 @@
-from configs import S, B, C
+from torchvision.models.feature_extraction import create_feature_extractor
+from torchvision.models import resnet18
 import torch.nn.functional as F
+from configs import S, B, C
 import torch.nn as nn
 import torch
-
-
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride):
-        super().__init__()
-
-        # Sequence 1: Convolutional Layer -> BatchNorm -> ReLU
-        self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=stride)
-        self.batch_norm_1 = nn.BatchNorm2d(out_channels)
-
-        # Sequence 2: Convolutional Layer -> BatchNorm -> shortcut + add -> ReLU
-        self.conv_2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
-        self.batch_norm_2 = nn.BatchNorm2d(out_channels)
-
-        # check if shortcut is required
-        if in_channels != out_channels or stride != 1:
-            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride)
-        else:
-            self.shortcut = nn.Identity()
-
-    def forward(self, x):
-        original_x = x
-
-        x = F.relu(self.batch_norm_1(self.conv_1(x)))
-        x = self.batch_norm_2(self.conv_2(x))
-
-        return F.relu(self.shortcut(original_x) + x)
-
-class ResidualBackbone(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        # Layer1
-        self.conv_1 = nn.Conv2d(3, 64, 7, padding=3)
-
-        # Residual Blocks
-        self.res_blocks = nn.ModuleList([])
-
-        self.res_blocks.append(ResidualBlock(64, 128, 2))
-        self.res_blocks.append(ResidualBlock(128, 128, 1))
-        self.res_blocks.append(ResidualBlock(128, 128, 2))
-        self.res_blocks.append(ResidualBlock(128, 256, 1))
-        self.res_blocks.append(ResidualBlock(256, 256, 2))
-        self.res_blocks.append(ResidualBlock(256, 256, 1))
-        self.res_blocks.append(ResidualBlock(256, 512, 2))
-        self.res_blocks.append(ResidualBlock(512, 512, 1))
-        self.res_blocks.append(ResidualBlock(512, 512, 2))
-
-    def forward(self, x):
-        x = F.relu(self.conv_1(x))
-
-        for res_block in self.res_blocks:
-            x = res_block(x)
-
-        return x
 
 class DetectorHead(nn.Module):
     def __init__(self):
@@ -71,11 +18,15 @@ class Model(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.backbone = ResidualBackbone()
+        # use resnet18 as the feature extractor/pretrained backbone
+        resnet = resnet18(weights="DEFAULT")
+        resnet_feature_extractor = create_feature_extractor(resnet, ["layer4"])
+
+        self.backbone = resnet_feature_extractor
         self.detector_head = DetectorHead()
 
     def forward(self, x):
-        x = self.backbone(x)
+        x = self.backbone(x)["layer4"]
         x = self.detector_head(x)
 
         x = x.reshape((x.shape[0], S, S, C + B * 5))
