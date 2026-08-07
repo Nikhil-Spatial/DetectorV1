937685bb15c64b19b3a5f644e05607ff4efae245
Nikhil Singh <nikhils1220@gmail.com>
2026-08-07 13:36:37 -0700

    Updated means and stds in transforms to imagenet's

--- src/transforms.py
+++ src/transforms.py
@@ -2,9 +2,9 @@ from torchvision.transforms import v2
 import torch
 
 # The mean and standard deviations across each channel for the normalized pixels
-# of every single image in the "trainval" dataset
-MEANS = (0.4485, 0.4249, 0.3922)
-STDS = (0.2682, 0.2655, 0.2782)
+# of every single image in the ImageNet1k Dataset
+MEANS = (0.485, 0.456, 0.406)
+STDS = (0.229, 0.224, 0.225)
 
 trainval_transforms = v2.Compose([
     v2.ColorJitter(
