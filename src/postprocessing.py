9fd07ea2d8464b25081f91474f992fa2d9878427
Nikhil Singh <nikhils1220@gmail.com>
2026-08-09 12:05:08 -0700

    finished the code for the project

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -1,6 +1,6 @@
-from src.configs import (S, C, B, IDX_TO_CLASS, CONFIDENCE_THRESHOLD,
+from configs import (S, C, B, IDX_TO_CLASS, CONFIDENCE_THRESHOLD,
                          NMS_IOU_THRESHOLD)
-from src.utilities import convert_xywh_coordinates, IoU
+from utilities import convert_xywh_coordinates, IoU
 from operator import itemgetter
 import torch
 
@@ -66,7 +66,7 @@ def non_maximum_suppression(filtered_grouped_preds):
     # order instead of increasing order.
     return suppressed_preds
 
-def postprocess_preds(preds, draw):
+def postprocess_preds(preds):
     # 1. Decode predictions
     decoded_preds = decode_preds(preds)
 
--- src/visualization.py
+++ src/visualization.py
@@ -7,7 +7,7 @@ def draw_rectangles(image, coords_list, labels):
     image = image.permute(1, 2, 0).numpy()
     image = bbv.draw_multiple_rectangles(image, coords_list, bbox_color=(255, 0, 0),
                                          thickness=1)
-    image = bbv.add_multiple_labels(image, labels, coords_list, size=0.5,
+    image = bbv.add_multiple_labels(image, labels, coords_list, size=0.4,
                                     thickness=1, draw_bg=False, text_color=
                                     (255, 255, 255))
 
